ed8013455950dc1550eb4210f792be29b97928b1
Ádám Báthori-Kiss <adam.bathori-kiss@lego.com>
2022-03-10 19:56:46 +0100

    Workoutfilter created

--- models/Workouts.model.js
+++ models/Workouts.model.js
@@ -2,8 +2,8 @@ const { Schema, model } = require("mongoose");
 
 const workoutSchema = new Schema({
   nameOfWorkout:  { type: String, required: true },
-  user: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  exercises:  [{ type: Schema.Types.ObjectId, ref: 'Exercises' }],
+  user: String,
+  exercises:  String,
   
 });
 
